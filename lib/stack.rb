773e2aa6fed897df8339c04d402ec858e8b8fe0d
Lianzi <lianziliu.sugar@gmail.com>
2022-12-14 17:26:44 -0500

    commit tree 424f8ed78ab64bd6d8d996169d3c517d04ee6005

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,44 @@
 # your code here
+class Stack
+
+    def initialize
+        @stack = []
+        # @stack.limit = value
+    end
+
+    def push value
+        @stack.push(value)
+        # unless @limit == 5
+        #   return  'Stack Overflow'
+        # end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        !stack.empty?
+    end
+
+    def search value
+        if @stack.include?(value)
+            return "include"
+        else
+            return -1
+        end
+    end
+
+end
